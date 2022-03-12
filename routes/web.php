4c620ae41ba0f299e7636e10a0659ff52c557666
tuansonhq <86151925+tuansonhq@users.noreply.github.com>
2022-03-12 12:10:57 +0700

    Update

---
+++ routes/web.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('conversation/{userId}', 'MessageController@conversation')
+    ->name('message.conversation');
+Route::post('send-message', 'MessageController@sendMessage')
+    ->name('message.send-message');
+
+Route::post('send-group-message', 'MessageController@sendGroupMessage')
+    ->name('message.send-group-message');
+
+Route::resource('message-groups', 'MessageGroupController');
+
+
+//Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
